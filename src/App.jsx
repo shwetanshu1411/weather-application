f00c520c0aa7c9299d25d35b70f5842fae528f75
shwetanshu1411 <2228149@kiit.ac.in>
2025-04-09 15:12:39 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import WeatherCard from './components/Weather';
 import ForecastCard from './components/forecast';
-import { motion } from 'framer-motion';
 import weather from './assets/weather1.jpeg'; // ✅ make sure this path is correct
 
 const MAX_SEARCH_HISTORY = 5;
